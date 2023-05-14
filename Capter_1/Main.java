a7ffdec06e201cd1c15ebd23240ca114e48b939b
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-14 06:32:21 +0000

    add

---
+++ Capter_1/Main.java
@@ -0,0 +1,11 @@
+/* サンプルプログラム Main
+   開発者:菅原　　作成日: 2020年4月
+*/
+public class Main {
+  // ここからmainメソッド
+  public static void main (String[] args) {
+    int age; // 年齢を入れる箱
+    age = 20;
+    System.out.println("私は" + age + "歳");
+  }
+}
