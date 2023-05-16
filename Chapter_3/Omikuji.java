e2e0d007ca88fefef60fd69e0ebc88ccb8dd628d
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-16 07:27:53 +0000

    add

--- Chapter_3/Omikuji.java
+++ Chapter_3/Omikuji.java
@@ -2,16 +2,19 @@ public class Omikuji {
   public static void main(String[] args) {
     System.out.println("あなたの運勢を占います");
     int fortune = new java.util.Random().nextInt(4) + 1;
-    if (fortune == 1) {
-      System.out.println("大吉");
-    } else if (fortune == 2) {
-      System.out.println("中吉");
-    } else if (fortune == 3) {
-      System.out.println("吉");
-    } else if (fortune == 4) {
-      System.out.println("凶")
-    } else {
-      System.out.println("凶")
+    switch (fortune) {
+      case 1:
+        System.out.println("大吉");
+        break;
+      case 2:
+        System.out.println("中吉");
+        break;
+      case 3:
+        System.out.println("吉");
+        break;
+      case 4:
+        System.out.println("凶");
+        break;
     }
   }
 }
