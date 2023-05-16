52e8f007a8bccb5fa48433465649be1224822a53
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-16 05:28:10 +0000

    add

---
+++ Capter_2/Print.java
@@ -0,0 +1,8 @@
+public class Print {
+  public static void main(String[] args) {
+    String name = "菅原";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+  }
+}
