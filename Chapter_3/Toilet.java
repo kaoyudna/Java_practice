c7a108c8a2f2b4aea3371e3d82b12e19dafdf565
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-16 07:17:39 +0000

    add

---
+++ Chapter_3/Toilet.java
@@ -0,0 +1,9 @@
+public class Toilet {
+  public static void main(String[] args) {
+    boolean doorClose = false;
+    while (doorClose == true) {
+      System.out.println("ドアをノックする");
+      System.out.println("１分待つ");
+    }
+  }
+}
---
+++ Chapter_3/Weather.java
@@ -0,0 +1,11 @@
+public class Weather {
+  public static void main(String[] args){
+    boolean weather = true;
+    if (weather == true) {
+      System.out.println("選択をします");
+      System.out.println("散歩に行きます");
+    } else {
+      System.out.println("DVDを見ます");
+    }
+  }
+}
